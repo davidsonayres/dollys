99addf95cd3a93801a3da92ced60a6b40f1891be
Michael <mjames.ayres@gmail.com>
2017-01-28 17:33:00 -0500

    Adjusted js - trying to render partials

--- index.js
+++ index.js
@@ -2,42 +2,38 @@ var $j = jQuery.noConflict();
 
 $j(function() {
   $j('#hours').hover(function() {
-    $j('body').css('background-image', 'url("images/DollysBackground2.jpg")');
-    // $j.ajax({
-    //   url: '_hours',
-    //   method: 'POST',
-    //   dataType: 'html'
-    // }).done(function(responseData){
-    //   console.log(responseData);
-    //   $j('#info').html(responseData);
-    // });
-    $j("#info").load("_hours");
-// });
+    $j('body').css('background-image', 'url("images/DollysBackground2.jpg")')
+    $j.ajax({
+      url: '_hours',
+      method: 'POST',
+      dataType: 'html'
+    }).done(function(responseData){
+      console.log(responseData);
+      $j('#info').html(responseData);
+    });
   });
 
   $j('#menus').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground3.jpg")')
-    // $j.ajax({
-    //   url: '_menus',
-    //   method: 'POST',
-    //   dataType: 'html'
-    // }).done(function(responseData){
-    //   console.log(responseData);
-    //   $j('#info').html(responseData);
-    // });
-    $j("#info").load("_menus");
+    $j.ajax({
+      url: '_menus',
+      method: 'POST',
+      dataType: 'html'
+    }).done(function(responseData){
+      console.log(responseData);
+      $j('#info').html(responseData);
+    });
   });
 
   $j('#contact').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground4.jpg")')
-    // $j.ajax({
-    //   url: '_contact',
-    //   method: 'POST',
-    //   dataType: 'html'
-    // }).done(function(responseData){
-    //   console.log(responseData);
-    //   $j('#info').html(responseData);
-    // });
-    $j("#info").load("_contact");
+    $j.ajax({
+      url: '_contact',
+      method: 'POST',
+      dataType: 'html'
+    }).done(function(responseData){
+      console.log(responseData);
+      $j('#info').html(responseData);
+    });
   });
 })
