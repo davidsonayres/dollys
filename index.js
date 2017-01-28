abc120b32ca0e23254ef85e42f7e742d2a692870
Michael <mjames.ayres@gmail.com>
2017-01-28 17:24:13 -0500

    Removed ajax calls, trying js to info on nav item hover

--- index.js
+++ index.js
@@ -2,38 +2,42 @@ var $j = jQuery.noConflict();
 
 $j(function() {
   $j('#hours').hover(function() {
-    $j('body').css('background-image', 'url("images/DollysBackground2.jpg")')
-    $j.ajax({
-      url: '_hours',
-      method: 'POST',
-      dataType: 'html'
-    }).done(function(responseData){
-      console.log(responseData);
-      $j('#info').html(responseData);
-    });
+    $j('body').css('background-image', 'url("images/DollysBackground2.jpg")');
+    // $j.ajax({
+    //   url: '_hours',
+    //   method: 'POST',
+    //   dataType: 'html'
+    // }).done(function(responseData){
+    //   console.log(responseData);
+    //   $j('#info').html(responseData);
+    // });
+    $("#info").load("_hours");
+// });
   });
 
   $j('#menus').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground3.jpg")')
-    $j.ajax({
-      url: '_menus',
-      method: 'POST',
-      dataType: 'html'
-    }).done(function(responseData){
-      console.log(responseData);
-      $j('#info').html(responseData);
-    });
+    // $j.ajax({
+    //   url: '_menus',
+    //   method: 'POST',
+    //   dataType: 'html'
+    // }).done(function(responseData){
+    //   console.log(responseData);
+    //   $j('#info').html(responseData);
+    // });
+    $("#info").load("_menus");
   });
 
   $j('#contact').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground4.jpg")')
-    $j.ajax({
-      url: '_contact',
-      method: 'POST',
-      dataType: 'html'
-    }).done(function(responseData){
-      console.log(responseData);
-      $j('#info').html(responseData);
-    });
+    // $j.ajax({
+    //   url: '_contact',
+    //   method: 'POST',
+    //   dataType: 'html'
+    // }).done(function(responseData){
+    //   console.log(responseData);
+    //   $j('#info').html(responseData);
+    // });
+    $("#info").load("_contact");
   });
 })
