d74bed3c175c0202a5c9adec2babb1c9a741a7d9
Michael <mjames.ayres@gmail.com>
2017-01-28 18:00:17 -0500

    Removed ajax calls, updated js - trying to load information in div on hover over nav item

--- index.js
+++ index.js
@@ -3,37 +3,40 @@ var $j = jQuery.noConflict();
 $j(function() {
   $j('#hours').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground2.jpg")')
-    $j.ajax({
-      url: 'hours',
-      method: 'PUT',
-      dataType: 'html'
-    }).done(function(responseData){
-      console.log(responseData);
-      $j('#info').html(responseData);
-    });
+    // $j.ajax({
+    //   url: 'hours',
+    //   method: 'PUT',
+    //   dataType: 'html'
+    // }).done(function(responseData){
+    //   console.log(responseData);
+    //   $j('#info').html(responseData);
+    // });
+    $j("#info").load("hours.html");
   });
 
   $j('#menus').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground3.jpg")')
-    $j.ajax({
-      url: 'menus',
-      method: 'PUT',
-      dataType: 'html'
-    }).done(function(responseData){
-      console.log(responseData);
-      $j('#info').html(responseData);
-    });
+    // $j.ajax({
+    //   url: 'menus',
+    //   method: 'PUT',
+    //   dataType: 'html'
+    // }).done(function(responseData){
+    //   console.log(responseData);
+    //   $j('#info').html(responseData);
+    // });
+    $j("#info").load("menus.html");
   });
 
   $j('#contact').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground4.jpg")')
-    $j.ajax({
-      url: 'contact',
-      method: 'PUT',
-      dataType: 'html'
-    }).done(function(responseData){
-      console.log(responseData);
-      $j('#info').html(responseData);
-    });
+    // $j.ajax({
+    //   url: 'contact',
+    //   method: 'PUT',
+    //   dataType: 'html'
+    // }).done(function(responseData){
+    //   console.log(responseData);
+    //   $j('#info').html(responseData);
+    // });
+    $j("#info").load("contact.html");
   });
 })
