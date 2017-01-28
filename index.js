2249446ca9d1d14c2366dd5fe6b2fb04982c1f88
Michael <mjames.ayres@gmail.com>
2017-01-28 17:14:47 -0500

    Added partials to render information on nav item hover, added dessert menu, updated js to render partials

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ var $j = jQuery.noConflict();
 $j(function() {
   $j('#hours').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground2.jpg")')
+    $j.ajax({
+      url: '_hours',
+      method: 'GET',
+      dataType: 'html'
+    }).done(function(responseData){
+      console.log(responseData);
+      $j('#info').html(responseData);
+    });
   });
 
   $j('#menus').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground3.jpg")')
+    $j.ajax({
+      url: '_menus',
+      method: 'GET',
+      dataType: 'html'
+    }).done(function(responseData){
+      console.log(responseData);
+      $j('#info').html(responseData);
+    });
   });
 
   $j('#contact').hover(function() {
     $j('body').css('background-image', 'url("images/DollysBackground4.jpg")')
+    $j.ajax({
+      url: '_contact',
+      method: 'GET',
+      dataType: 'html'
+    }).done(function(responseData){
+      console.log(responseData);
+      $j('#info').html(responseData);
+    });
   });
 })
