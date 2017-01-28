844a1bfeaf1bbb8ef5aa4e8cbc5c0bc1491a9336
Michael <mjames.ayres@gmail.com>
2017-01-28 18:06:57 -0500

    Cleaned up js, adjusted styling for info

--- index.js
+++ index.js
@@ -2,41 +2,17 @@ var $j = jQuery.noConflict();
 
 $j(function() {
   $j('#hours').hover(function() {
-    $j('body').css('background-image', 'url("images/DollysBackground2.jpg")')
-    // $j.ajax({
-    //   url: 'hours',
-    //   method: 'PUT',
-    //   dataType: 'html'
-    // }).done(function(responseData){
-    //   console.log(responseData);
-    //   $j('#info').html(responseData);
-    // });
+    $j('body').css('background-image', 'url("images/DollysBackground2.jpg")');
     $j("#info").load("hours.html");
   });
 
   $j('#menus').hover(function() {
-    $j('body').css('background-image', 'url("images/DollysBackground3.jpg")')
-    // $j.ajax({
-    //   url: 'menus',
-    //   method: 'PUT',
-    //   dataType: 'html'
-    // }).done(function(responseData){
-    //   console.log(responseData);
-    //   $j('#info').html(responseData);
-    // });
+    $j('body').css('background-image', 'url("images/DollysBackground3.jpg")');
     $j("#info").load("menus.html");
   });
 
   $j('#contact').hover(function() {
-    $j('body').css('background-image', 'url("images/DollysBackground4.jpg")')
-    // $j.ajax({
-    //   url: 'contact',
-    //   method: 'PUT',
-    //   dataType: 'html'
-    // }).done(function(responseData){
-    //   console.log(responseData);
-    //   $j('#info').html(responseData);
-    // });
+    $j('body').css('background-image', 'url("images/DollysBackground4.jpg")');
     $j("#info").load("contact.html");
   });
 })
