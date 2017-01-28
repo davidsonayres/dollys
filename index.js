cc1e8a2bceaaf5ff98197635c3880c1facf8c26d
Michael <mjames.ayres@gmail.com>
2017-01-28 18:11:17 -0500

    Updated js from hover to on click

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var $j = jQuery.noConflict();
 
 $j(function() {
-  $j('#hours').hover(function() {
+  $j('#hours').on('click', function() {
     $j('body').css('background-image', 'url("images/DollysBackground2.jpg")');
     $j("#info").load("hours.html");
   });
 
-  $j('#menus').hover(function() {
+  $j('#menus').on('click', function() {
     $j('body').css('background-image', 'url("images/DollysBackground3.jpg")');
     $j("#info").load("menus.html");
   });
 
-  $j('#contact').hover(function() {
+  $j('#contact').on('click', function() {
     $j('body').css('background-image', 'url("images/DollysBackground4.jpg")');
     $j("#info").load("contact.html");
   });
