67ceda20a62bdb3f5b8550772ca51e43538ff937
Michael <mjames.ayres@gmail.com>
2017-01-28 19:26:03 -0500

    Adjusted js to slide in bookenda widget and blur page when reservations is clicked

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ var $j = jQuery.noConflict();
 
 $j(function() {
   $j('#hours').on('click', function() {
-    $j('body').css('background-image', 'url("images/DollysBackground2.jpg")');
+    $j('#page_wrapper').css('background-image', 'url("images/DollysBackground2.jpg")');
     $j("#info").load("hours.html");
   });
 
   $j('#menus').on('click', function() {
-    $j('body').css('background-image', 'url("images/DollysBackground3.jpg")');
+    $j('#page_wrapper').css('background-image', 'url("images/DollysBackground3.jpg")');
     $j("#info").load("menus.html");
   });
 
   $j('#contact').on('click', function() {
-    $j('body').css('background-image', 'url("images/DollysBackground4.jpg")');
+    $j('#page_wrapper').css('background-image', 'url("images/DollysBackground4.jpg")');
     $j("#info").load("contact.html");
   });
+
+  $j('#reservations').on('click', function() {
+    $j('#reservations_wrapper').css('margin-top', 0);
+    $j('#page_wrapper').css('filter', 'blur(3px)')
+  });
+
+  $j('#reservations_wrapper').on('click', function() {
+    $j('#reservations_wrapper').css('margin-top', '100%');
+    $j('#page_wrapper').css('filter', 'blur(0px)')
+  });
+
 })
