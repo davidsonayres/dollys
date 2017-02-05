7052932f0b78840add3a2e5a6ceed4c633738c9a
Michael <mjames.ayres@gmail.com>
2017-02-05 17:46:16 -0500

    Adjusted view after menu button pressed, adjusted js for menu touch event

--- index.js
+++ index.js
@@ -1,32 +1,76 @@
 var $j = jQuery.noConflict();
+var mql = window.matchMedia("(min-width: 768px)");
 
-$j(function() {
-  $j('#hours').on('click', function() {
-    $j('#page_wrapper').css('background-image', 'url("images/DollysBackground2.jpg")');
-    $j("#info").load("hours.html");
-  });
+if (mql.matches) {
+  /* the viewport is at least 400 pixels wide */
+  $j(function() {
+    $j('#hours').on('click', function() {
+      $j('#page_wrapper').css('background-image', 'url("images/DollysBackground2.jpg")');
+      $j("#info").load("hours.html");
+    });
 
-  $j('#menus').on('click', function() {
-    $j('#page_wrapper').css('background-image', 'url("images/DollysBackground3.jpg")');
-    $j("#info").load("menus.html");
-  });
+    $j('#menus').on('click', function() {
+      $j('#page_wrapper').css('background-image', 'url("images/DollysBackground3.jpg")');
+      $j("#info").load("menus.html");
+    });
 
-  $j('#contact').on('click', function() {
-    $j('#page_wrapper').css('background-image', 'url("images/DollysBackground4.jpg")');
-    $j("#info").load("contact.html");
-  });
+    $j('#contact').on('click', function() {
+      $j('#page_wrapper').css('background-image', 'url("images/DollysBackground4.jpg")');
+      $j("#info").load("contact.html");
+    });
 
-  $j('#reservations').on('click', function() {
-    $j('#reservations_wrapper').css('margin-top', 0);
-    $j('#page_wrapper').css('filter', 'blur(3px)')
-  });
+    $j('#reservations').on('click', function() {
+      $j('#reservations_wrapper').css('margin-top', 0);
+      $j('#page_wrapper').css('filter', 'blur(3px)')
+    });
 
-  $j('#reservations_wrapper').on('click', function(e) {
-    if (e.target !== this)
-      return;
-    else
-      $j('#reservations_wrapper').css('margin-top', '100%');
-      $j('#page_wrapper').css('filter', 'blur(0px)');
-  });
+    $j('#reservations_wrapper').on('click', function(e) {
+      if (e.target !== this)
+        return;
+      else
+        $j('#reservations_wrapper').css('margin-top', '100%');
+        $j('#page_wrapper').css('filter', 'blur(0px)');
+    });
+  })
+} else {
+  /* the viewport is less than 400 pixels wide */
+  $j(function() {
 
-})
+    $j('div.menu_button').on('click', function() {
+      $j('div.dolly').css('bottom', '-62%');
+      $j('div.dolly').css('right', '-25%');
+      $j('img.slogan').attr('src', 'images/Dollys_Logo.svg');
+      $j('img.slogan').css('width', '20%');
+      $j('img.slogan').css('margin', '-5% auto -6%');
+      $j('nav').css('display', 'inline-block');
+    });
+
+    $j('#hours').on('click', function() {
+      $j('#page_wrapper').css('background-image', 'url("images/DollysBackground2.jpg")');
+      $j("#info").load("hours.html");
+    });
+
+    $j('#menus').on('click', function() {
+      $j('#page_wrapper').css('background-image', 'url("images/DollysBackground3.jpg")');
+      $j("#info").load("menus.html");
+    });
+
+    $j('#contact').on('click', function() {
+      $j('#page_wrapper').css('background-image', 'url("images/DollysBackground4.jpg")');
+      $j("#info").load("contact.html");
+    });
+
+    $j('#reservations').on('click', function() {
+      $j('#reservations_wrapper').css('margin-top', 0);
+      $j('#page_wrapper').css('filter', 'blur(3px)')
+    });
+
+    $j('#reservations_wrapper').on('click', function(e) {
+      if (e.target !== this)
+        return;
+      else
+        $j('#reservations_wrapper').css('margin-top', '100%');
+        $j('#page_wrapper').css('filter', 'blur(0px)');
+    });
+  })
+}
